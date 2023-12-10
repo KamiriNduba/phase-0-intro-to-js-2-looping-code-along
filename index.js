584b2b141cd383b4a22b0583d2da5dc3a14d2d58
NDUBA <user@users-MacBook-Pro.local>
2023-12-10 18:53:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Test calls for writeCards function
+  writeCards(["Ada", "Brendan", "Ali"], "birthday");
+  
+  // Test call for countDown function
+  countDown(10);
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
